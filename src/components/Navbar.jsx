a1c79d2b498649a95112083cf7ace74411fdf570
Aditya <aditya.khazanchi@gmail.com>
2024-04-29 02:05:47 +0530

    toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {logo, menu, close} from '../assets';
 
 const Navbar = () => {
   const [active, setactive] = useState("");
+  const [toggle, settoggle] = useState(false);
   return (
     <nav className= {'${styles.paddingX} w-full flex items-center py-5  fixed top-0 z-20 bg-primary'}>
 
@@ -23,7 +24,10 @@ const Navbar = () => {
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
-            <li>
+            <li 
+            key = {link.id} className={`${ active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} 
+                      onClick={() => setActive(link.title)}
+                      >
               <a
                 href={'#${nav.id}'}>{link.title}
               </a>
@@ -31,11 +35,18 @@ const Navbar = () => {
           ))}
 
         </ul>
-
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src = {toggle ? close : menu}
+                alt = "menu"
+                className='w-[28px] h-[28px]
+                object-contain cursor-pointer'
+                onClick={() => setToggle=(!toggle)}/>
+          
+        </div>
       </div>
 
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar    
